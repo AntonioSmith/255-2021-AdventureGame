2db0c02fcd344aaf8113189eb39894e4db757cd7
Antonio <wapasmith@gmail.com>
2021-02-03 09:44:31 -0500

    Torch and Camera Fix
    + Torch model redone
    + Camera Vertical movement fixed

--- Assets/Scripts/PlayerHeadTurn.cs
+++ Assets/Scripts/PlayerHeadTurn.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerHeadTurn : MonoBehaviour
 {
-    public float turnSpeed = 60;
+    public float turnSpeed = 70;
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        float vertical = Input.GetAxis("Vertical") * turnSpeed * Time.deltaTime;
-        transform.Rotate(0, horizontal, 0);
-        transform.Rotate(vertical, 0, 0);
+        float vertical = Input.GetAxis("Vertical") * turnSpeed / -1 * Time.deltaTime;
+        transform.Rotate(vertical, horizontal, 0);
     }
 }
